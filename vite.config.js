881256f98de4d6ffb3de56b9a06f675652b3a8ea
carodriguezp <crpzgz92@gmail.com>
2024-03-04 10:08:52 +0100

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/carodriguezp/Fit-pig_Website",
+  base: "/Fit-pig_Website",
 })
