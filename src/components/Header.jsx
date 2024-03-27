dfbae22f6baa5caf4b7ebdc42c2226405416c4ff
carodriguezp <crpzgz92@gmail.com>
2024-03-27 16:00:35 +0100

    header updated with component menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
+import Menu from "./Menu"
 
 
 function Header() {
     return (
         <header className="header">
-
             <div className="header__div">
-                <img src="src\images\pigFace.png" alt="" />
-                <h1>Fit Pig</h1>
+                <div className="header__div-div">
+                    <img src="src\images\pigFace.png" alt="" />
+                    <h1>Fit Pig</h1>
+                </div>
+
+                <Menu />
             </div>
 
             <h2>Even if you feel fat, you can be fit</h2>
