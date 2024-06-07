bed22816f36f6d28a45cc826c37b42ce4f35bcbf
carodriguezp <crpzgz92@gmail.com>
2024-06-07 12:49:49 +0200

    create global styles and theme for variables

---
+++ src/theme.js
@@ -0,0 +1,27 @@
+export const theme = {
+
+//fonts
+fontFamilyGluten: '"Gluten", cursive',
+fontWeightGluten: 400,
+
+fontFamilySister: '"Love Ya Like A Sister", cursive',
+fontWeightSister: 400,
+
+fontFamilyLuckiestguy: '"Luckiest Guy", cursive',
+fontWeightLuckiestguy: 400,
+
+fontFamilyRoboto: '"Roboto", sansserif',
+
+//colors
+
+colorPalePink: '#ffe6f2',
+colorSoftPink: '#ffcce6',
+colorClairePink: '#ff99cc',
+
+colorTruePink: '#ff3399',
+colorStrongPink:' #e60073',
+colorDarkPink:' #b30059',
+
+colorWine: '#660033',
+colorBurgundy: '#33001a',
+  }
