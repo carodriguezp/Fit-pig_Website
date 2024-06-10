69a8153ebbf2c5489f7537209e71b6d9d7783004
carodriguezp <crpzgz92@gmail.com>
2024-06-10 15:46:17 +0200

    initial style burguer menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,65 @@
-import MenuList from "./MenuList"
+import styled from "styled-components";
+
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../../globalStyles';
+import { theme } from '../../theme';
+
+import MenuList from "./MenuList";
+
+const MenuHeader = styled.div `
+
+/* background-color: ${({ theme }) => theme.colorPalePink}; */
+
+    background-color:white;
+    width: 100%;
+
+
+
+    i {
+        font-size: 2.2rem;
+        color: ${({ theme }) => theme.colorWine};
+
+        
+
+        @media (min-width: 768px) {
+            font-size: 3.5rem;
+        }
+
+        @media (min-width: 1022px) {
+            font-size: 3.5rem;
+        }
+
+    }
+
+    i.fa-bars{
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            width: 100vw;
+            display: flex;
+            flex-direction: row-reverse;
+        }
+
+    `;
 
 
 function Menu() {
     return (
 
-        <div className="menu">
+        <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+
+        <MenuHeader className="menu">
+        
             <i className="fa-solid fa-bars"></i>
 
             <MenuList />
 
-        </div>
+        </MenuHeader>
 
+        </>
+    </ThemeProvider>
     )
 }
 
--- src/components/Menu/MenuList.jsx
+++ src/components/Menu/MenuList.jsx
@@ -1,12 +1,53 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const MenuListHeader = styled.ul `
+    list-style: none;
+    background: #333;
+    width: 100%;
+    height: 50%;
+    position:fixed;
+    right: 0px;
+    z-index: 1000;
+    border-radius: 20px;
+    box-shadow: 0px 0px 20px 8px ${({ theme }) => theme.colorWine};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 20px;
+
+    transition-timing-function: cubic-bezier(10,2,3,1);
+    //transform: translateX(50rem);
+    //top: 0;
+    top: 11%;
+    z-index: 0;
+    transition: 0.5s;
+    
+    @media (min-width: 768px) {
+        top: 18%;
+        width: 80%;
+        right: 10%;
+        padding: 35px;
+    }
+
+    @media (min-width: 1022px) {
+            top: 25%;
+            width: 80%;
+            right: 10%;
+            padding: 50px;
+    }
+
+   
+       `;
+
 
 function MenuList() {
     return (
-        <ul className="menu-list">
-            <li className="menu-list-element"><Link to="/"><i className="fa-solid fa-house hover">Home</i></Link></li>
-            <li className="menu-list-element"><Link to="/about"><i className="fa-solid fa-address-card hover">About Fit Pig</i></Link></li>
-            <li className="menu-list-element"><Link to="/improve"><i className="fa-solid fa-dumbbell hover">Want to improve?</i></Link></li>
-        </ul>
+        <MenuListHeader className="menu-list">
+            <li className="menu-list-element"><Link to="/"><i className="fa-solid fa-house hover">    Home</i></Link></li>
+            <li className="menu-list-element"><Link to="/about"><i className="fa-solid fa-address-card hover">    About Fit Pig</i></Link></li>
+            <li className="menu-list-element"><Link to="/improve"><i className="fa-solid fa-dumbbell hover">    Want to improve?</i></Link></li>
+        </MenuListHeader>
     )
 }
 
